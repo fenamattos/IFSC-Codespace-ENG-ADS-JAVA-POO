653ed837d0e9da4382589452a0e8bdcbca7ebadd
fenamattos <94716415+fenamattos@users.noreply.github.com>
2025-02-06 23:03:39 +0000

    fernanda

---
+++ Aula.java
@@ -0,0 +1,17 @@
+public class Aula 
+    {
+        public static void main(String[] args) {
+            int a = 100;
+            int b = 200;
+           
+            if (a == 100) {
+                // SE sim então
+                if (b == 200) {
+                    // SE sim então
+                    System.out.println("Valor de a é 100 e b é 200");
+                }
+            }
+        }
+     }
+    
+
